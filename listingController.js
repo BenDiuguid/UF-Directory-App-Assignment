ae8877e045215dec1496eeb2d11d434eaf98f01f
Ben Diuguid <Bendiuguid@gmail.com>
2016-01-12 15:20:41 -0500

    functionally complete, ready to be made pretty

--- listingController.js
+++ listingController.js
@@ -1,14 +1,42 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.newListing = {
+      coordinates: {
+        latitude: 0,
+        longitude: 0
+      }
+    };
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
-     */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    var clearForm = function() {
+      $scope.newListing.code = '';
+      $scope.newListing.name = '';
+      $scope.newListing.coordinates.latitude = 0;
+      $scope.newListing.coordinates.longitude = 0;
+      $scope.newListing.address = '';
+    };
+
+    clearForm();
+
+    $scope.addListing = function() {
+      var newListingToInsert = {
+        name: $scope.newListing.name,
+        code: $scope.newListing.code,
+        address: $scope.newListing.address,
+        coordinates: {
+          latitude: $scope.newListing.coordinates.latitude,
+          longitude: $scope.newListing.coordinates.longitude
+        }
+      }
+      $scope.listings.push(newListingToInsert);
+      clearForm();
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
